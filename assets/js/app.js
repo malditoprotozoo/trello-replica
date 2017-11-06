01f859d4481cde4c345478e1f9a0408e4006c2f6
malditoprotozoo <virodriguez.fe@gmail.com>
2017-11-06 20:06:03 -0300

    Version 0.0.1 ready! Now the text dissapears onclick and appears a form instead

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,20 @@
+// Estableciendo variables
+
+var boards = document.getElementById("boards");
+var addList = document.getElementById("add-list");
+var mainBoard = document.getElementById("main-board");
+var items = document.getElementById("items");
+var buttonAdd = document.getElementById("add");
+var buttonClose = document.getElementById("close");
+
+// Función que transformará #add-list en #main-board
 function transforms() {
-	
+	addList.addEventListener("click", formAppears);
+}
+
+function formAppears() {
+	addList.classList.add("invisible");
+	mainBoard.classList.remove("invisible");
 }
 
 
