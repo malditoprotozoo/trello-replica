2db11f3bf418e35a61a3a03b25e165d8b7b51aab
malditoprotozoo <virodriguez.fe@gmail.com>
2017-11-07 19:51:21 -0300

    Version 0.0.3

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,53 +11,87 @@ var buttonAdd = document.getElementById("add");
 
 // Función que remplazará #add-list por #main-board
 function transforms() {
-	addList.addEventListener("click", formAppears);
+    addList.addEventListener("click", formAppears);
 };
 
 function formAppears() {
-	addList.classList.add("invisible");
-	mainBoard.classList.remove("invisible");
-	mainBoard.classList.add("visible");
-	addList.classList.remove("visible");
-	buttonSave.addEventListener("click", saveNewList);
-	buttonClose.addEventListener("click", close);
+    addList.classList.add("invisible");
+    mainBoard.classList.remove("invisible");
+    mainBoard.classList.add("visible");
+    addList.classList.remove("visible");
+    buttonSave.addEventListener("click", saveNewList);
+    buttonClose.addEventListener("click", close);
 };
 
 function close() {
-	mainBoard.classList.remove("visible");
-	mainBoard.classList.add("invisible");
-	addList.classList.remove("invisible");
-	addList.classList.add("visible");
+    mainBoard.classList.remove("visible");
+    mainBoard.classList.add("invisible");
+    addList.classList.remove("invisible");
+    addList.classList.add("visible");
 };
 
 // event.preventDefault() evita que el botón realice alguna acción pretederminada.
 function saveNewList() {
-	event.preventDefault();
-	var titleBoard = document.getElementById("title-list").value;
-	document.getElementById("title-list").value = "";
-	if (titleBoard !== "") {
-		// A continuación se crea el nuevo elemento y su título.
-		var aNewBoard = document.createElement("form");
-		var containerTitle = document.createElement("h2");
-		var textTitle = document.createTextNode(titleBoard);
-		containerTitle.appendChild(textTitle);
-		aNewBoard.appendChild(containerTitle);
-		boards.insertBefore(aNewBoard, boards.childNodes[0]);
-		aNewBoard.classList.add("new-board");
+    event.preventDefault();
+    var titleBoard = document.getElementById("title-list").value;
+    document.getElementById("title-list").value = "";
 
-		// Aquí se creará el input
-		var newInput = document.createElement("input");
-		aNewBoard.appendChild(newInput);
-		newInput.setAttribute("placeholder", "Añadir una tarjeta...");
+    if (titleBoard !== "") {
+        // A continuación se crea el nuevo elemento y su título.
+        var aNewBoard = document.createElement("form");
+        var containerTitle = document.createElement("h2");
+        var textTitle = document.createTextNode(titleBoard);
+        containerTitle.appendChild(textTitle);
+        aNewBoard.appendChild(containerTitle);
+        boards.insertBefore(aNewBoard, mainBoard);
+        aNewBoard.classList.add("new-board");
 
-		// Aquí se crearán el botón de añadir
-		var textAddButton = document.createTextNode("Añadir");
-		var newButton = document.createElement("button");
-		newButton.appendChild(textAddButton);
-		aNewBoard.appendChild(newButton);
-		newButton.classList.add("button");
-	}
-}
+        // Determinar una ID que corresponda a la ubicación del elemento padre
+    	var location = document.getElementsByTagName("form").length - 1;
+    	aNewBoard.setAttribute("id", location);
+
+        // Aquí se creará el input
+        var newInput = document.createElement("input");
+        aNewBoard.appendChild(newInput);
+        newInput.setAttribute("placeholder", "Añadir una tarjeta...");
+        newInput.setAttribute("id", "new-input");
+
+        newInput.addEventListener("click", createItem);
+
+    }
+};
+
+function createItem() {
+    event.preventDefault();
+    document.getElementById("new-input").classList.add("invisible");
+    
+    var board = document.getElementsByClassName("new-board")[0];
+    var textArea = document.createElement("textarea");
+    board.appendChild(textArea);
+
+    // Aquí se creará el botón de añadir
+    var textAddButton = document.createTextNode("Añadir");
+    var newButton = document.createElement("button");
+    newButton.appendChild(textAddButton);
+    board.appendChild(newButton);
+    newButton.classList.add("button");
+
+    // Aquí se creará el botón de cerrar
+    var newCloseButton = document.createElement("button");
+    var spanClose = document.createElement("span");
+    newCloseButton.appendChild(spanClose);
+    spanClose.classList.add("icon-close");
+    newCloseButton.classList.add("closing-button");
+    board.appendChild(newCloseButton);
+
+    newCloseButton.addEventListener("click", cancelNewList);
+};
+
+function cancelNewList() {
+    event.preventDefault();
+
+    this.parentElement.remove();
+};
 
 // Luego de añadir esa función, podemos iniciar la nueva función que queremos establecer.
 
@@ -78,10 +112,10 @@ function saveNewList() {
 
 
 // var containerItem = document.createElement("div");
-		// var contentItem = document.createElement("p");
-		// textItem = document.createTextNode(textItem);
-		// containerItem.appendChild(textItem);
-		// containerItem.appendChild(contentItem);
+// var contentItem = document.createElement("p");
+// textItem = document.createTextNode(textItem);
+// containerItem.appendChild(textItem);
+// containerItem.appendChild(contentItem);
 
 // function addNewList(content, buttonAdd) {
 // 	var where = buttonAdd.parentElement.parentElement;
@@ -101,3 +135,10 @@ function saveNewList() {
 // 	where.addEventListener("drop", drop);
 // }
 
+
+// var findId = 1;
+//     var idString = findId.toString();
+//     var board = document.getElementById(idString);
+
+//     // Aquí se creará el área de texto
+//     var board = document.getElementById(idString);
