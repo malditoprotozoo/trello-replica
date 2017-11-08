6f2585f7372eb3a7a383dcd1ba3b1a42882081f9
malditoprotozoo <virodriguez.fe@gmail.com>
2017-11-08 01:39:42 -0300

    Version 0.0.4

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,11 +3,11 @@
 var boards = document.getElementById("boards");
 var addList = document.getElementById("add-list");
 var mainBoard = document.getElementById("main-board");
-// var items = document.getElementById("items");
 var buttonSave = document.getElementById("save");
 var buttonClose = document.getElementById("close");
 var toDoList = document.getElementById("todo-list");
 var buttonAdd = document.getElementById("add");
+var cards = [];
 
 // Función que remplazará #add-list por #main-board
 function transforms() {
@@ -30,61 +30,104 @@ function close() {
     addList.classList.add("visible");
 };
 
+// Uso la variable this, para que lo indicado a continuación se limite a un elemento
+
+function card(title) {
+
+    let parent = this;
+
+    this.aNewBoard = document.createElement("form");
+    this.containerTitle = document.createElement("h2");
+    this.textTitle = document.createTextNode(title);
+
+    this.containerTitle.appendChild(this.textTitle);
+    this.aNewBoard.appendChild(this.containerTitle);
+
+    boards.insertBefore(this.aNewBoard, mainBoard);
+
+    this.aNewBoard.classList.add("new-board");
+
+    // Determinar una ID que corresponda a la ubicación del elemento padre
+    this.myID = document.getElementsByTagName("form").length - 1;
+    this.aNewBoard.setAttribute("id", this.myID);
+
+    // Aquí se creará el input 
+    this.newInput = document.createElement("input");
+
+    this.newInput.setAttribute("placeholder", "Añadir una tarjeta...");
+    this.newInput.setAttribute("id", "input_" + this.myID);
+    this.aNewBoard.appendChild(this.newInput);
+
+    this.newInput.addEventListener("click", function(event) {
+        event.preventDefault();
+        document.getElementById("input_" + parent.myID).style.display = "none";
+
+        var textArea = document.createElement("textarea");
+        parent.aNewBoard.appendChild(textArea);
+        textArea.focus();
+
+        // Aquí se creará el botón de añadir
+        var textAddButton = document.createTextNode("Añadir");
+        var newButton = document.createElement("button");
+        newButton.appendChild(textAddButton);
+        parent.aNewBoard.appendChild(newButton);
+        newButton.classList.add("button");
+        // this.newButton.setAttribute("id", "newButton" + this.myID);
+
+        // Aquí se creará el botón de cerrar
+        var newCloseButton = document.createElement("button");
+        var spanClose = document.createElement("span");
+        newCloseButton.appendChild(spanClose);
+        spanClose.classList.add("icon-close");
+        newCloseButton.classList.add("closing-button");
+        parent.aNewBoard.appendChild(newCloseButton);
+
+        newCloseButton.addEventListener("click", function(event) {
+            event.preventDefault();
+
+            this.parentElement.removeChild(textArea);
+            this.parentElement.removeChild(newButton);
+            this.parentElement.removeChild(newCloseButton);
+            document.getElementById("input_" + parent.myID).style.display = "inline-block";
+            // document.getElementById("input_" + parent.myID).classList.add("visible");
+
+        })
+
+        newButton.addEventListener("click", function(event) {
+            event.preventDefault();
+
+            // Si el contenido de el área de texto está vacío
+            // no se creará un nuevo item
+            
+            if (textArea.value !== "") {
+            var containerItem = document.createElement("p");
+            var contentItem = document.createTextNode(textArea.value);
+
+            containerItem.appendChild(contentItem);
+            containerItem.classList.add("item");
+            this.parentElement.insertBefore(containerItem, document.getElementById("input_" + parent.myID));
+
+            textArea.value = "";
+            textArea.focus();
+        	}
+        })
+
+    })
+
+};
+
 // event.preventDefault() evita que el botón realice alguna acción pretederminada.
-function saveNewList() {
+function saveNewList(event) {
     event.preventDefault();
     var titleBoard = document.getElementById("title-list").value;
     document.getElementById("title-list").value = "";
 
+    // Si no se inserta un texto, no pasaremos a la función y por ende
+    // El botón "guardar" estará desabilitado
+
     if (titleBoard !== "") {
-        // A continuación se crea el nuevo elemento y su título.
-        var aNewBoard = document.createElement("form");
-        var containerTitle = document.createElement("h2");
-        var textTitle = document.createTextNode(titleBoard);
-        containerTitle.appendChild(textTitle);
-        aNewBoard.appendChild(containerTitle);
-        boards.insertBefore(aNewBoard, mainBoard);
-        aNewBoard.classList.add("new-board");
-
-        // Determinar una ID que corresponda a la ubicación del elemento padre
-    	var location = document.getElementsByTagName("form").length - 1;
-    	aNewBoard.setAttribute("id", location);
-
-        // Aquí se creará el input
-        var newInput = document.createElement("input");
-        aNewBoard.appendChild(newInput);
-        newInput.setAttribute("placeholder", "Añadir una tarjeta...");
-        newInput.setAttribute("id", "new-input");
-
-        newInput.addEventListener("click", createItem);
-
-    }
-};
-
-function createItem() {
-    event.preventDefault();
-    document.getElementById("new-input").classList.add("invisible");
-    
-    var board = document.getElementsByClassName("new-board")[0];
-    var textArea = document.createElement("textarea");
-    board.appendChild(textArea);
-
-    // Aquí se creará el botón de añadir
-    var textAddButton = document.createTextNode("Añadir");
-    var newButton = document.createElement("button");
-    newButton.appendChild(textAddButton);
-    board.appendChild(newButton);
-    newButton.classList.add("button");
-
-    // Aquí se creará el botón de cerrar
-    var newCloseButton = document.createElement("button");
-    var spanClose = document.createElement("span");
-    newCloseButton.appendChild(spanClose);
-    spanClose.classList.add("icon-close");
-    newCloseButton.classList.add("closing-button");
-    board.appendChild(newCloseButton);
-
-    newCloseButton.addEventListener("click", cancelNewList);
+		cards.push(new card(titleBoard));
+	}
 };
 
 function cancelNewList() {
@@ -93,52 +136,15 @@ function cancelNewList() {
     this.parentElement.remove();
 };
 
-// Luego de añadir esa función, podemos iniciar la nueva función que queremos establecer.
-
-// function addItem() {
-// 	event.preventDefault();
-// 	var textItem = document.getElementById("items").value;
-// 	document.getElementById("items").value = "";
-// 	if (textItem !== "") {
-// 		var contentItem = document.createElement("li");
-// 		var textInsideItem = document.createTextNode(textItem);
-// 		contentItem.appendChild(textInsideItem);
-// 		toDoList.appendChild(contentItem);
-// 	}
-// };
-
-
-
-
-
-// var containerItem = document.createElement("div");
-// var contentItem = document.createElement("p");
-// textItem = document.createTextNode(textItem);
-// containerItem.appendChild(textItem);
-// containerItem.appendChild(contentItem);
-
-// function addNewList(content, buttonAdd) {
-// 	var where = buttonAdd.parentElement.parentElement;
-// 	var item = document.createElement("div");
-// 	var newList = document.createElement("div");
-
-// 	newList.innerText = content.value;
-// 	newList.classList.add("insert-name-here");
-// 	where.insertBefore(newList, where.childNodes[0]);
-
-// 	item.innerText = "Añadir una tarjeta...";
-// 	item.classList.add("item");
-// 	where.appendChild(item);
-
-// 	item.addEventListener("click", boardAppears);
-// 	where.addEventListener("dragover", drag);
-// 	where.addEventListener("drop", drop);
-// }
-
-
-// var findId = 1;
-//     var idString = findId.toString();
-//     var board = document.getElementById(idString);
-
-//     // Aquí se creará el área de texto
-//     var board = document.getElementById(idString);
+function addItem() {
+    event.preventDefault();
+    var textItem = document.getElementsByTagName("textarea")[0].value;
+    document.getElementsByTagName("textarea").value = "";
+    if (textItem !== "") {
+        var board = document.getElementsByClassName("new-board")[0];
+        var contentItem = document.createElement("span");
+        var textInsideItem = document.createTextNode(textItem);
+        contentItem.appendChild(textInsideItem);
+        board.appendChild(contentItem);
+    }
+};
